fc85be23ed6bd53ff418b2b8ae6b74a14642625e
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2025-04-22 16:38:14 +0530

    Minor Changes

--- src/app/components/Herosection.tsx
+++ src/app/components/Herosection.tsx
@@ -98,7 +98,7 @@ export function HeroSectionOne() {
           </button>
           <button
             className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900"
-            onClick={() => window.open("https://github.com/VA5UDEV", "_blank")}
+            onClick={() => window.open("https://github.com/VA5UDEV/clerk-startup-temp", "_blank")}
           >
             Github Repo
           </button>
